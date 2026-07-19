827fc12f8bd8bbbcebffbf5e394701b1ce66deee
migwynkriid <sebastijanaqw@gmail.com>
2026-07-20 00:48:05 +0200

    Revert "feat: Add Chatter feature modules (LinkHover, UrlCopy, CopyChat, AltNames, Highlight, ClickInvite)"
    This reverts commit 51bd4bfc130cc1d6493483b82120fe41e78da4ad.

--- Core/Core.lua
+++ Core/Core.lua
@@ -246,12 +246,6 @@ local defaults = {
 		status = true,
 		systemmessages = false,
 		tradeskills = true,
-		linkHover = true,
-		urlCopy = true,
-		copyChat = true,
-		altNames = true,
-		highlight = true,
-		clickInvite = true,
 	},
 }
 
